d17cf835e52013c65d26ab207b44fb4be22fb398
kartikeywastaken <kartikeyjuyal18@gmail.com>
2025-08-22 12:11:23 +0530

    changes to guess c

--- guess.c
+++ guess.c
@@ -4,21 +4,36 @@
 #include <string.h> 
 
 int main(){
-    char guessU = 'n';
-    int i = 1;
+    char guessU = 'n'; // initialising the guessU by giving it value 'n'
+    int i= 1; // i = 0
+    int count = 0;
     printf("Choose a number b/w 1 and 10 and ill try to  guess it\n");
-    while ( guessU != 'y')
+    while ( guessU != 'y' && count < 10)   // continues untill guess is not y
     {
-
         printf("Is your number %d\n", i);
         scanf(" %c", &guessU);
 
-        if (guessU == 'n')
+        if (guessU == 'y')
+        {
+            printf("The number you chose is: %d", i);
+            return 0;
+        } 
+        else if (guessU == 'n')
+        {
+            i++;
+            count++;
+        }
+        else
+        {
+            printf("Please enter a valid input! \n");
+            continue;
+        }
+    }
+    if (count == 10)
+
     {
-        i++;
+        printf("Sorry i couldn't guess the number");
     } 
-    } 
-    printf("Then The number you guessed is: %d", i);
-    
+
     return 0;
 }
