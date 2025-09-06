77ead6d9a5e7945afbb2e0baeef96b699902039f
kartikeywastaken <kartikeyjuyal18@gmail.com>
2025-09-06 19:09:16 +0530

    a game. small one but it works fine. yes it can be improved but not right now.

--- dungeon_game.c
+++ dungeon_game.c
@@ -24,17 +24,16 @@ char map[10][10] = {
         {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
 };
 
-void position(struct user_data posi){ // to locate users position.
-    for ( int i = 0; i < 10; i++)
+void position(struct user_data posi){ // to locate the player's position.
+    for ( int i = 0; i < 10; i++) // takes a struct as an argument.
     {
         for ( int j = 0; j < 10; j++)
         {
-            if (j == posi.x_cor && i == posi.y_cor)
-            {
+            if (j == posi.x_cor && i == posi.y_cor) // when it iterates through the loop 
+            { // at the point where posi.x and posi.y is same it will print "@"
                 printf("@");
             } else {
-                printf("Invalid position!");
-                printf(" %c", map[i][j]);
+                printf("%c", map[i][j]);
             }
             
         }
@@ -60,11 +59,11 @@ struct user_data moveD(struct user_data p){ // a function using struct. which ta
 }
 
 struct user_data moveS(struct user_data p){
-  int new_y =  p.y_cor + 1; 
+  int new_y =  p.y_cor + 1;  // made a temp variable to check if move is valid or not.
   int new_x = p.x_cor;                                
-  if (new_x >= 0 && new_x <= 9 && map[new_y][new_x] != '#')
+  if (new_x >= 0 && new_x <= 9 && map[new_y][new_x] != '#') // using new_x and new_y to check 
   {
-    p.y_cor++;
+    p.y_cor++;                                   // the validity of moves which are to be made
     return p; // return the updated p struct 
   } else {
     printf("Out of bounds!");
@@ -86,8 +85,8 @@ struct user_data moveW(struct user_data p){
 }
 
 struct user_data moveA(struct user_data p){
-  int new_y =  p.x_cor - 1;
-  int new_x = p.y_cor;
+  int new_x = p.x_cor - 1;
+  int new_y = p.y_cor;
   if (new_x >= 0 && new_x <= 9 && map[new_y][new_x] != '#')
   {
     p.x_cor--;
@@ -124,18 +123,35 @@ int main(){
     {
         printf("Enter player name: ");
         scanf(" %s", player1.user_name);
-
+        printf("Your current health is: 100 \n");
+        player1.user_health = 100;
 
         player1.x_cor = 1;
         player1.y_cor = 1;
 
-        while (player1.user_health > 0 && player1.x_cor != 7 && player1.y_cor != 6)
+        while (player1.user_health > 0)
         {
-            printf("Your current health is: 100 \n");
-            player1.user_health = 100;
-
+            
+            // if (player1.x_cor == 7 && player1.y_cor == 6)
+            // {
+            //   printf("Congratulations, you found the exit!\n");
+            //   break;
+            // }
+          
+            position(player1);
             printf("Enter w/a/s/d to move!: \n");
-            scanf("%c", player1.user_move);
+            scanf(" %c", &player1.user_move);
+
+            if (map[player1.y_cor][player1.x_cor] == 'E')
+            {
+              printf("Congratulations, you found the exit!\n");
+              break;
+            }
+            if (map[player1.y_cor][player1.x_cor] == 'T')
+            {
+              printf("You fell into a trap! You lose half your health\n");
+              player1.user_health = player1.user_health/2;
+            }
 
             if (player1.user_move == 'w')
             {
@@ -146,22 +162,22 @@ int main(){
             {
                 player1 = moveA(player1);
                 position(player1);
+                
             }
             if (player1.user_move == 's')
             {
                 player1 = moveS(player1);
                 position(player1);
+                
             }
             if (player1.user_move == 'd')
             {
                 player1 = moveD(player1);
-                position(player1); // trying to print the updated map 
-            }
-        
-        
-            
+                position(player1);
+                // trying to print the updated map 
+            }   
         }
-        printf("Congratulations, you found the exit!\n");
+         printf("You died! \n");
         
         
     };
